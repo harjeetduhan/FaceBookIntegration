983d082f9c2b961646bee10efd7a6821f108dd33
macserver <macserver@macservers-mac-pro.local>
2016-08-31 16:02:24 +0530

    Initial Commit

---
+++ FacebookIntegration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacebookIntegration
+//
+//  Created by macserver on 8/31/16.
+//  Copyright © 2016 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacebookIntegration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacebookIntegration
+//
+//  Created by macserver on 8/31/16.
+//  Copyright © 2016 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
